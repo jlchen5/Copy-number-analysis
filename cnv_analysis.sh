4ddc37c1f9367b293269ad7a5e909acf11cc4077
Jiale Chen <61578993+231Charlie@users.noreply.github.com>
2021-08-05 19:00:04 +0800

    Add files via upload

---
+++ cnv_analysis.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "          Info of this Script: Designed by Jiale Chen"
+echo "          Attention: If you have some problems, Please contact chenjiale@sklmg.edu.cn"
+
+echo "          Doing Sequence Align"
+
+
+cd Rawdata/
+ls *gz |sed 's/_[12].fastq.gz//'|sort -u > ../sample.txt
+cd ..
+
+if [ ! -d bam ];then
+    echo "mkdir bam"
+    mkdir -p bam
+fi 
+
+echo "          Mapping"
+# Mapping 
+cat sample.txt|while read line ;do bowtie2 -x /home/reference/Dmelanogaster/UCSC/dm6/Sequence/Bowtie2Index/dm6 -p 20 -1 Rawdata/${line}_1.fastq.gz -2 Rawdata/${line}_2.fastq.gz -S ${line}.sam ;done
+
+echo "          sam to bam conversion"
+# sam to bam conversion  
+samtools view -bSq 10 ${line}.sam > ${line}.bam
+
+echo "          bam to bed conversion"
+# bam to bed conversion  
+bamToBed -i ${line}.bam | cut -f 1,2,3,4,5,6 | sort -T . -k1,1 -k2,2n -S 5G > ${line}.bed
+
+echo "          bed line number calculate  "
+# bed line number calculate  
+x=`wc -l ${line}.bed | cut -d' ' -f 1`
+
+echo "          generate coverage on genomic windows"
+# generate coverage on genomic windows  
+bedtools intersect -sorted -c -b ${line}.bed -a /home/chenjiale/project/cnv/dm6_windows_50k.bed |awk -vx=$x '{print $1,$2,$3,$4*1e+06/x}' OFS='\t' > ${line}.bg
+
+
+
+echo End time: `date`
